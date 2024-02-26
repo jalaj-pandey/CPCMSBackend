9929aa776dce860df996d38743463e99eca74e7e
Jalaj Pandey <jalajpandey123@gmail.com>
2024-02-26 23:59:03 +0530

    first commit

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,8 +1,12 @@
 import {NextFunction, Request, Response} from "express";
 import {User} from "../models/user.js"
-export const newUser = async (req: Request, res: Response, next: NextFunction)=>{
+import { NewUserRequestBody } from "../types/types.js";
+export const newUser = async (
+    req: Request<{},{},NewUserRequestBody>, 
+    res: Response, 
+    next: NextFunction)=>{
     try {
-        const {} =  req.body;
+        const {fname, lname,photo,gender } =  req.body;
         const user = await User.create({})
             return res.status(200).json({
                 success:true,
---
+++ src/types/types.ts
@@ -0,0 +1,17 @@
+
+
+
+
+export interface NewUserRequestBody {
+    _id: string;
+    fname: string;
+    lname: string;
+    email: string;
+    photo: string;
+    role: "admin" | "user";
+    gender: "male" | "female";
+    dob: Date;
+    createdAt: Date;
+    updatedAt: Date;
+    age: number;
+}
