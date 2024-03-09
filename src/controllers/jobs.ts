0080f336bb6ab6f0aaf2663c00e5d7ee2984e592
Jalaj Pandey <jalajpandey123@gmail.com>
2024-03-09 16:16:39 +0530

    Reduced Server load time Commit

--- src/controllers/jobs.ts
+++ src/controllers/jobs.ts
@@ -1,6 +1,10 @@
 import { Request } from "express";
 import { TryCatch } from "../middlewares/error.js";
-import { BaseQuery, NewJobRequestBody, searchRequestQuery } from "../types/types.js";
+import {
+  BaseQuery,
+  NewJobRequestBody,
+  searchRequestQuery,
+} from "../types/types.js";
 import { Jobs } from "../models/jobs.js";
 import ErrorHandler from "../types/utility-class.js";
 import { rm } from "fs";
@@ -8,11 +12,12 @@ import { myCache } from "../app.js";
 
 export const newJob = TryCatch(
   async (req: Request<{}, {}, NewJobRequestBody>, res, next) => {
-    const { _id, name, title, description, requirements, salary, location } = req.body;
+    const { _id, name, title, description, requirements, salary, location } =
+      req.body;
     const photo = req.file;
 
     if (!photo) return next(new ErrorHandler("Please provide a photo", 400));
-    
+
     if (
       !name ||
       !title ||
@@ -45,16 +50,15 @@ export const newJob = TryCatch(
   }
 );
 
+// Revalidate on New, Update and Delete jobs
 export const getLatestJobs = TryCatch(async (req, res, next) => {
-
   let jobs;
-  if(myCache.has("latest-jobs")) 
-  jobs = JSON.parse(myCache.get("latest-jobs") as string);
-  else{
+  if (myCache.has("latest-jobs"))
+    jobs = JSON.parse(myCache.get("latest-jobs") as string);
+  else {
     jobs = await Jobs.find({}).sort({ createdAt: -1 }).limit(5);
     myCache.set("latest-jobs", JSON.stringify(jobs));
   }
-   
 
   return res.status(201).json({
     success: true,
@@ -62,8 +66,15 @@ export const getLatestJobs = TryCatch(async (req, res, next) => {
   });
 });
 
+// Revalidate on New, Update and Delete jobs
 export const getAdminJobs = TryCatch(async (req, res, next) => {
-  const jobs = await Jobs.find({});
+  let jobs;
+  if (myCache.has("admin-jobs"))
+    jobs = JSON.parse(myCache.get("admin-jobs") as string);
+  else {
+    jobs = await Jobs.find({});
+    myCache.set("admin-jobs", JSON.stringify(jobs));
+  }
   return res.status(201).json({
     success: true,
     jobs,
@@ -71,8 +82,15 @@ export const getAdminJobs = TryCatch(async (req, res, next) => {
 });
 
 export const getSingleJob = TryCatch(async (req, res, next) => {
-  const job = await Jobs.findById(req.params.id);
-  if(!job) return next(new ErrorHandler("Job not found", 404))
+  let job;
+  const id = req.params.id;
+  if (myCache.has(`job-${id}`))
+    job = JSON.parse(myCache.get(`job-${id}`) as string);
+  else {
+    const job = await Jobs.findById(id);
+    if (!job) return next(new ErrorHandler("Job not found", 404));
+    myCache.set(`job-${id}`, JSON.stringify(job));
+  }
 
   return res.status(201).json({
     success: true,
@@ -80,106 +98,94 @@ export const getSingleJob = TryCatch(async (req, res, next) => {
   });
 });
 
-export const updateJob = TryCatch(
-  async (req, res, next) => {
-    const id = req.params.id;
-    const { name, title, description, requirements, salary, location } = req.body;
-    const photo = req.file;
-    const job = await Jobs.findById(id);
+export const updateJob = TryCatch(async (req, res, next) => {
+  const id = req.params.id;
+  const { name, title, description, requirements, salary, location } = req.body;
+  const photo = req.file;
+  const job = await Jobs.findById(id);
 
-    if(!job) return next(new ErrorHandler("Job not found", 404))
+  if (!job) return next(new ErrorHandler("Job not found", 404));
 
-
-    if (photo) {
-      rm(job.photo!, () => {
-        console.log("Old photo deleted");
-      });
-      job.photo = photo.path;
-    }
-
-
-    if(name) job.name = name;
-    if(title) job.title = title;
-    if(description) job.description = description;
-    if(requirements) job.requirements = requirements;
-    if(salary) job.salary = salary;
-    if(location) job.location = location;
-
-    await job.save();
-
-    return res.status(200).json({
-      success: true,
-      Message: "Job updated successfully",
+  if (photo) {
+    rm(job.photo!, () => {
+      console.log("Old photo deleted");
     });
+    job.photo = photo.path;
   }
-);
 
+  if (name) job.name = name;
+  if (title) job.title = title;
+  if (description) job.description = description;
+  if (requirements) job.requirements = requirements;
+  if (salary) job.salary = salary;
+  if (location) job.location = location;
 
+  await job.save();
+
+  return res.status(200).json({
+    success: true,
+    Message: "Job updated successfully",
+  });
+});
 
 export const deleteJob = TryCatch(async (req, res, next) => {
   const job = await Jobs.findById(req.params.id);
-  if(!job) return next(new ErrorHandler("Job Not Found", 404))
+  if (!job) return next(new ErrorHandler("Job Not Found", 404));
 
   rm(job.photo!, () => {
-        console.log("Job photo deleted");
-      });
+    console.log("Job photo deleted");
+  });
   await job.deleteOne();
   return res.status(201).json({
     success: true,
     Message: "Job deleted successfully",
-
   });
 });
-
-
-
 
 export const getAllJobs = TryCatch(
-  async (req: Request<{},{},{},searchRequestQuery>, res, next) => {
+  async (req: Request<{}, {}, {}, searchRequestQuery>, res, next) => {
+    const { search, sort, salary, company } = req.query;
 
-  const {search,sort,salary,company} = req.query;
+    const page = Number(req.query.page) || 1;
 
-  const page = Number(req.query.page) || 1;
+    const limit = Number(process.env.JOBS_PER_PAGE) || 8;
+    const skip = limit * (page - 1);
 
-  const limit = Number(process.env.JOBS_PER_PAGE) || 8;
-  const skip = limit*(page - 1);
+    const baseQuery: BaseQuery = {};
 
+    if (search)
+      baseQuery.title = {
+        $regex: search,
+        $options: "i",
+      };
 
-  const baseQuery: BaseQuery = {
-    
-    
+    if (salary)
+      baseQuery.salary = {
+        $lte: Number(salary),
+      };
+
+    if (company)
+      baseQuery.name = {
+        $regex: company,
+        $options: "i",
+      };
+
+    const jobsPromise = Jobs.find(baseQuery)
+      .sort(sort && { salary: sort === "asc" ? 1 : -1 })
+      .limit(limit)
+      .skip(skip);
+
+    const [jobs, filteredOnlyJobs] = await Promise.all([
+      jobsPromise,
+      Jobs.find({ baseQuery }),
+    ]);
+
+    const totalPage = Math.ceil(filteredOnlyJobs.length / limit);
+
+    return res.status(201).json({
+      success: true,
+      jobs,
+      totalPage,
+    });
   }
-
-  if(search) baseQuery.title = {
-    $regex: search, 
-    $options: "i",
-  };
-
-  if(salary) baseQuery.salary = {
-    $lte: Number(salary),
-  }
-
-  if(company) baseQuery.name = {
-    $regex: company, 
-    $options: "i",
-  }
-
-  const jobsPromise = Jobs.find(baseQuery).sort(
-    sort && {salary: sort === "asc" ? 1: -1}
-    ).limit(limit).skip(skip);
-
-  const [jobs,filteredOnlyJobs] = await Promise.all([
-    jobsPromise,
-    Jobs.find({baseQuery})
-  ])
-    
-    const totalPage = Math.ceil(filteredOnlyJobs.length/limit);
-
-  return res.status(201).json({
-    success: true,
-    jobs,
-    totalPage,
-  });
-});
-
-
+);
